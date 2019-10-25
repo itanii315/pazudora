374ff1c58c0467d0150840d9369ac0aba2613acc
Itani <>
2019-10-25 09:48:29 +0900

    update

--- pazu/main.py
+++ pazu/main.py
@@ -1,13 +1,17 @@
 import pygame
-from pygame.locals import QUIT, MOUSEBUTTONDOWN, MOUSEMOTION, MOUSEBUTTONUP
+from pygame.locals import QUIT, MOUSEBUTTONDOWN, MOUSEMOTION, MOUSEBUTTONUP, KEYDOWN
+from pygame.locals import K_1, K_2, K_3, K_4, K_5, K_6, K_z, K_x
 import sys
 import random
 import glob
 
-SCREEN_SIZE = (480, 840)
-N_DROP_X = 6
-N_DROP_Y = 5
+sys.setrecursionlimit(5000)
+
+SCREEN_SIZE = (400, 600)
+N_DROP_X = 6 * 10
+N_DROP_Y = 5 * 10
 FPS = 60
+COMBO_INTERVAL = int(FPS * 0.4)
 DROP_LENGTH = SCREEN_SIZE[0] // N_DROP_X
 OFFSET_Y = SCREEN_SIZE[1] - DROP_LENGTH * N_DROP_Y
 
@@ -16,7 +20,7 @@ MODE_ERASE = 1
 
 pygame.mixer.init(44100, -16, 2, 512)
 pygame.init()
-pygame.mixer.set_num_channels(16)
+pygame.mixer.set_num_channels(64)
 
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("パズドラゲーム")
@@ -25,6 +29,7 @@ pygame.mixer.music.load("bgm_02.ogg")
 pygame.mixer.music.play(-1)
 SOUNDS = {filename: pygame.mixer.Sound(filename) for filename in glob.glob("se_*.ogg")}
 IMAGES = {filename: pygame.transform.smoothscale(pygame.image.load(filename).convert_alpha(), (DROP_LENGTH, DROP_LENGTH)) for filename in glob.glob("*.png")}
+FONT = pygame.font.Font("ipaexg.ttf", 40)
 
 class Pazudora:
     def __init__(self):
@@ -62,6 +67,9 @@ class Pazudora:
                 self.draw_drop(pos, self.drops[y][x])
         if self.is_moving:
             self.draw_drop(self.moving_drop_pos, self.moving_drop_type)
+
+        sprite = FONT.render(f"{self.erase_combo}コンボ", True, (255, 255, 255))
+        screen.blit(sprite, (20, 20))
         pygame.display.update() # 画面更新
 
     def erase_motion(self):
@@ -83,7 +91,7 @@ class Pazudora:
             chain = self.erase_drops.pop(0)
             for x, y in chain:
                 self.drops[y][x] = 0
-            self.erase_motion_count = int(FPS * 0.4)
+            self.erase_motion_count = COMBO_INTERVAL
         else:
             if self.is_in_zero():
                 # 落下
@@ -91,14 +99,17 @@ class Pazudora:
                 self.fall_new_drop()
                 self.erase_drops = self.erase_drop()
                 if self.erase_drops:
-                    self.erase_motion_count = int(FPS * 0.4)
+                    self.erase_motion_count = COMBO_INTERVAL
                 else:
                     self.finish_erase()
             else:
                 self.finish_erase()
 
-    def finish_erase(self):
+    def start_erase(self):
         self.erase_combo = 0
+        self.erase_mode = MODE_ERASE
+
+    def finish_erase(self):
         self.erase_motion_count = 0
         self.erase_mode = MODE_NORMAL
 
@@ -109,6 +120,43 @@ class Pazudora:
             self.mouse_move_action(event)
         if event.type == MOUSEBUTTONUP and event.button == 1:
             self.mouse_up_action(event)
+        if event.type == KEYDOWN:
+            if self.is_moving or self.erase_mode == MODE_ERASE:
+                return
+            if event.key == K_z:
+                self.drops_to_max_combo()
+            if event.key == K_x:
+                self.drops_to_can_all_clear()
+            if event.key == K_1:
+                self.drops_to_hanabi(1)
+            if event.key == K_2:
+                self.drops_to_hanabi(2)
+            if event.key == K_3:
+                self.drops_to_hanabi(3)
+            if event.key == K_4:
+                self.drops_to_hanabi(4)
+            if event.key == K_5:
+                self.drops_to_hanabi(5)
+            if event.key == K_6:
+                self.drops_to_hanabi(6)
+
+    def drops_to_can_all_clear(self):
+        choices = [(i // 3) % 6 + 1 for i in range(N_DROP_X * N_DROP_Y)]
+        for y in range(N_DROP_Y):
+            for x in range(N_DROP_X):
+                index = random.randrange(len(choices))
+                self.drops[y][x] = choices.pop(index)
+
+    def drops_to_max_combo(self):
+        choices = [(i // 3) % 6 + 1 for i in range(N_DROP_X * N_DROP_Y)]
+        for y in range(N_DROP_Y):
+            for x in range(N_DROP_X):
+                self.drops[y][x] = choices.pop(0)
+
+    def drops_to_hanabi(self, n):
+        for y in range(N_DROP_Y):
+            for x in range(N_DROP_X):
+                self.drops[y][x] = n
 
     def mouse_down_action(self, event):
         if self.erase_mode == MODE_NORMAL:
@@ -135,8 +183,7 @@ class Pazudora:
         if self.is_moving:
             self.is_moving = False
             self.drops[self.moving_drop_index[1]][self.moving_drop_index[0]] = self.moving_drop_type
-            # erase
-            self.erase_mode = MODE_ERASE
+            self.start_erase()
 
     def draw_drop(self, pos, n):
         left_up = pos[0] - DROP_LENGTH // 2, pos[1] - DROP_LENGTH // 2
